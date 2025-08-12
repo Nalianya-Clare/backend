1296c4dcfd718444502082ae7da7721ff17f7906
nalianya-Clare <nalianyaclare@gmail.com>
2025-08-12 13:11:07 +0300

    update

--- src/settings.py
+++ src/settings.py
@@ -285,7 +285,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:4173",
     "chrome-extension://njbpngbkjfjmoadndnbneaejachkcdpm"
 ]
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = False
 
 SESSION_COOKIE_SECURE = False
